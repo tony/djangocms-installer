289b8c19be874d210a5f8ac05d351ff2a5367b01
Iacopo Spalletti <i.spalletti@nephila.it>
2014-12-23 23:13:08 +0100

    more setup

--- tests/base.py
+++ tests/base.py
@@ -86,5 +86,5 @@ class IsolatedTestClass(BaseTestClass):
                 with open(activate_temp) as f:
                     code = compile(f.read(), activate_temp, 'exec')
                 exec(code, dict(__file__=activate_temp))
-            subprocess.check_call(['pip', 'install', 'pip==1.5.6', "'setuptools<8'"])
+            subprocess.check_call(['pip', 'install', 'pip==1.5.6'])
             sys.executable = os.path.join(self.virtualenv_dir, 'bin', 'python')
